7dd79ebc0b6dd2adb7eae167aa47c2c52f3c8dea
Jeremy Brown <brown471@tesla.cs.iupui.edu>
2016-04-14 18:36:07 -0400

    Add deadlock-detector.cpp. Initialize pointers to arrays and 2D arrays.

---
+++ deadlock-detector.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int* totalResources;
+	int* processes;
+	int* available = new int[*resources];
+	int** allocation = new int*[*processes];
+	int** request = new int*[*processes];
+	int* allocatedResources = new int[*totalResources];
+	int* requestedResources = new int[*totalResources];	
+
+	void readFile();
+	
+	return 0;
+}//end main function
