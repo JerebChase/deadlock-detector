711e207c05bf9f67156e11d2e91ac3a344d13d81
Jeremy Brown <brown471@tesla.cs.iupui.edu>
2016-04-21 18:31:14 -0400

    Add functions to read in data and resources available.

--- deadlock-detector.cpp
+++ deadlock-detector.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void loadResources(int* totalResources, int* processes, int** allocation) {
+void loadData(int* totalResources, int* processes) {
 	ifstream readFile;
 	string data;
 	readFile.open("data.txt");
@@ -23,19 +23,53 @@ void loadResources(int* totalResources, int* processes, int** allocation) {
 	if (data[length] != ',') {
 		*totalResources = *totalResources + 1;
 	}//end if statement
+}//end function
+
+void loadResources(int* totalResources, int* processes, int* available, int** allocation) {
+	ifstream readFile;
+	string data;
+	string resource = "";
+	readFile.open("data.txt");
+	getline(readFile, data, ';');
+	getline(readFile, data);
+	int position = 0;
+	for (int i = 0; i < data.length(); i++) {
+		if (data[i] == ',') {
+			available[position] = atoi(resource.c_str());
+			position++;
+			resource = "";
+		}//end if statement
+		else if (data[i] != ',') {
+			resource += data[i];
+		}//end else if statement
+	}//end for loop
+	int length = data.length();
+	if (data[length] != ',') {
+		available[position] = atoi(resource.c_str());
+	}//end if statement
 }//end readFile function
 
 int main() {
 	int totalResources = 0;
 	int processes = 0;
-	int* available;// = new int[*totalResources];
-	int** allocation;// = new int*[*processes];
+	int* available;
+	int** allocation;
 	int** request;// = new int*[*processes];
 	int* allocatedResources;// = new int[*totalResources];
 	int* requestedResources;// = new int[*totalResources];
 
-	loadResources(&totalResources, &processes, allocation);
+	loadData(&totalResources, &processes);
+	available = new int[totalResources];
+	allocation = new int*[processes];
+	for (int i = 0; i < processes; i++) {
+		allocation[i] = new int[totalResources];
+	}//end for loop
+	loadResources(&totalResources, &processes, available, allocation);
 	cout << "Number of processes: " << processes << endl;
 	cout << "Number of resources: " << totalResources << endl;
+	cout << "Available resources: ";
+	for (int i = 0; i < totalResources; i++) {
+		cout << available[i] << ", ";
+	}//end for loop
 	return 0;
 }//end main function
