79690575af95172859dca4a360b0dad03409cbd9
Jeremy Brown <brown471@tesla.cs.iupui.edu>
2016-04-21 09:40:36 -0400

    Create function to read in process and resource totals from file.

--- deadlock-detector.cpp
+++ deadlock-detector.cpp
@@ -8,28 +8,34 @@
 using namespace std;
 
 void loadResources(int* totalResources, int* processes, int** allocation) {
-	ofstream readFile;
+	ifstream readFile;
 	string data;
 	readFile.open("data.txt");
-	getline(readFile, string);
-	cout << string << endl;
+	getline(readFile, data, ';');
+	*processes = atoi(data.c_str());
+	getline(readFile, data);
+	for (int i = 0; i < data.length(); i++) {
+		if (data[i] == ',') {
+			*totalResources = *totalResources + 1;
+		}//end if statement
+	}//end for loop
+	int length = data.length();
+	if (data[length] != ',') {
+		*totalResources = *totalResources + 1;
+	}//end if statement
 }//end readFile function
 
 int main() {
-	int* totalResources;
-	int* processes;
-	int* available = new int[*totalResources];
-	int** allocation = new int*[*processes];
-	int** request = new int*[*processes];
-	int* allocatedResources = new int[*totalResources];
-	int* requestedResources = new int[*totalResources];	
+	int totalResources = 0;
+	int processes = 0;
+	int* available;// = new int[*totalResources];
+	int** allocation;// = new int*[*processes];
+	int** request;// = new int*[*processes];
+	int* allocatedResources;// = new int[*totalResources];
+	int* requestedResources;// = new int[*totalResources];
 
-	loadResources(totalResources, processes, allocation);
-	
-	delete totalResources;
-	delete processes;
-	delete available;
-	delete allocatedResources;
-	delete requestedResources;
+	loadResources(&totalResources, &processes, allocation);
+	cout << "Number of processes: " << processes << endl;
+	cout << "Number of resources: " << totalResources << endl;
 	return 0;
 }//end main function
