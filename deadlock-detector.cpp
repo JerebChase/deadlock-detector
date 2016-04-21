e1a2a9891dd3dffb4899204fc649d36542203668
Jeremy Brown <brown471@tesla.cs.iupui.edu>
2016-04-21 07:29:04 -0400

    Edit deadlock-detector.cpp.

--- deadlock-detector.cpp
+++ deadlock-detector.cpp
@@ -7,16 +7,29 @@
 
 using namespace std;
 
+void loadResources(int* totalResources, int* processes, int** allocation) {
+	ofstream readFile;
+	string data;
+	readFile.open("data.txt");
+	getline(readFile, string);
+	cout << string << endl;
+}//end readFile function
+
 int main() {
 	int* totalResources;
 	int* processes;
-	int* available = new int[*resources];
+	int* available = new int[*totalResources];
 	int** allocation = new int*[*processes];
 	int** request = new int*[*processes];
 	int* allocatedResources = new int[*totalResources];
 	int* requestedResources = new int[*totalResources];	
 
-	void readFile();
+	loadResources(totalResources, processes, allocation);
 	
+	delete totalResources;
+	delete processes;
+	delete available;
+	delete allocatedResources;
+	delete requestedResources;
 	return 0;
 }//end main function
